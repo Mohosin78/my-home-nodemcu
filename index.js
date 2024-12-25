50c3d3462710f2902fd17b7ea745f6caf9edb826
Md.Mohosin Ali <74634015+Mohosin78@users.noreply.github.com>
2024-12-25 22:31:52 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const pin_array=[2,4,5,12,13,14,15,16],pin_array_state=[0,0,0,0,0,0,0,0],pin_detail=["My Room","Dinning","kitchen","Bathroom","park","Room 3","Stair","Gate"];
 function toggleLED(_e) {
-  console.log(pin_array_state[_e.state])
-  let _o = _e.ID,
-    $state = pin_array_state[_e.state] == 0 ? 1 : 0;
-
+  let _o = _e.ID;
+  console.log(pin_array_state[_e.state]);
+  let $state = pin_array_state[_e.state] == 0 ? 1 : 0;
+  console.log($state)
   let _eo = _e.children[1].classList;
   _eo.add("s");
-
-  console.log(_e,_e.state)
 
   fetch("/toggle?led=" + _o + "&&state=" + $state)
     .then((response) => {
@@ -18,7 +16,6 @@ function toggleLED(_e) {
         _eo.remove("s");
         _eo.toggle("a");
         _e.state = $state;
-        console.log(_e,_e.state,$state);
       }
     })
     .catch((error) => {
